e3416fe3f30d0200b1dccb523975591f65035447
JLChnToZ <jeremylam619@gmail.com>
2021-03-01 20:59:53 +0800

    Tweak values and add breathe idle animation.

--- src/worker/vrm-idle-helper.ts
+++ src/worker/vrm-idle-helper.ts
@@ -7,15 +7,19 @@ import { clampByRadian } from '../utils/three-helpers';
 const BoneNames = VRMSchema.HumanoidBoneName;
 
 const LERP_SCALE = 6;
+const HEAD_ROTATE_DAMP = 1;
 const HEAD_CLAMP_ANGLE = 60 * MathUtils.DEG2RAD;
 const ARM_IDLE_ANGLE = 75 * MathUtils.DEG2RAD;
 const FINGER_IDLE_ANGLE = 15 * MathUtils.DEG2RAD;
 const THUMB_IDLE_ANGLE = 30 * MathUtils.DEG2RAD;
+const SHOULDER_BREATH_ANGLE = -5 * MathUtils.DEG2RAD;
 const MIN_BLINK_DELAY = 0.5;
-const MAX_BLINK_DEALY = 20;
+const MAX_BLINK_DEALY = 10;
 const BLINK_DURATION = 0.2;
+const BREATH_CYCLE = 2;
 
 const lookAts = new WeakMap<VRM, VRMLookAtHead>();
+const totalTimes = new WeakMap<VRM, number>();
 const blinkDelays = new WeakMap<VRM, number>();
 
 const vector3 = new Vector3();
@@ -29,11 +33,16 @@ const euler = new Euler();
 const leftArmIdleRotation = getQuaternionFromAngle(0, 0, ARM_IDLE_ANGLE);
 const leftFingerIdleRotation = getQuaternionFromAngle(0, 0, FINGER_IDLE_ANGLE);
 const leftThumbIdleRotation = getQuaternionFromAngle(0, THUMB_IDLE_ANGLE, 0);
+const leftShoulderBreathRotation = getQuaternionFromAngle(0, 0, SHOULDER_BREATH_ANGLE);
+const leftArmBreathRotation = getQuaternionFromAngle(0, 0, ARM_IDLE_ANGLE - SHOULDER_BREATH_ANGLE);
 const righArmIdleRotation = getQuaternionFromAngle(0, 0, -ARM_IDLE_ANGLE);
 const rightFingerIdleRotation = getQuaternionFromAngle(0, 0, -FINGER_IDLE_ANGLE);
 const rightThumbIdleRotation = getQuaternionFromAngle(0, -THUMB_IDLE_ANGLE, 0);
+const rightShoulderBreathRotation = getQuaternionFromAngle(0, 0, -SHOULDER_BREATH_ANGLE);
+const righArmBreathRotation = getQuaternionFromAngle(0, 0, SHOULDER_BREATH_ANGLE - ARM_IDLE_ANGLE);
 
 const idlePose = new Map<VRMSchema.HumanoidBoneName, Quaternion>([
+  [BoneNames.LeftShoulder, new Quaternion()],
   [BoneNames.LeftUpperArm, leftArmIdleRotation],
   [BoneNames.LeftIndexDistal, leftFingerIdleRotation],
   [BoneNames.LeftIndexIntermediate, leftFingerIdleRotation],
@@ -50,6 +59,7 @@ const idlePose = new Map<VRMSchema.HumanoidBoneName, Quaternion>([
   [BoneNames.LeftThumbDistal, leftThumbIdleRotation],
   [BoneNames.LeftThumbIntermediate, leftThumbIdleRotation],
   [BoneNames.LeftThumbProximal, leftFingerIdleRotation],
+  [BoneNames.RightShoulder, new Quaternion()],
   [BoneNames.RightUpperArm, righArmIdleRotation],
   [BoneNames.RightIndexDistal, rightFingerIdleRotation],
   [BoneNames.RightIndexIntermediate, rightFingerIdleRotation],
@@ -66,6 +76,12 @@ const idlePose = new Map<VRMSchema.HumanoidBoneName, Quaternion>([
   [BoneNames.RightThumbDistal, rightThumbIdleRotation],
   [BoneNames.RightThumbIntermediate, rightThumbIdleRotation],
   [BoneNames.RightThumbProximal, rightFingerIdleRotation],
+]);
+const breathPose = new Map<VRMSchema.HumanoidBoneName, Quaternion>([
+  [BoneNames.LeftShoulder, leftShoulderBreathRotation],
+  [BoneNames.LeftUpperArm, leftArmBreathRotation],
+  [BoneNames.RightShoulder, rightShoulderBreathRotation],
+  [BoneNames.RightUpperArm, righArmBreathRotation],
 ]);
 
 function getQuaternionFromAngle(x: number, y: number, z: number) {
@@ -84,6 +100,7 @@ export function unregisterModel(model: VRM) {
   if (!lookAt) return;
   lookAt.target = undefined;
   lookAts.delete(model);
+  totalTimes.delete(model);
 }
 
 export function updateModel(model: VRM, deltaTime: number) {
@@ -98,25 +115,22 @@ function updateHead(model: VRM, deltaTime: number) {
   if (!bone) return;
   bone.matrixWorld.decompose(position, rotation, vector3);
   position2.setFromMatrixPosition(camera.matrixWorld);
-  euler.setFromQuaternion(
-    rotation2.copy(rotation).slerp(
-      rotation3.setFromRotationMatrix(
-        matrix.lookAt(position, position2, vector3.set(0, 1, 0)),
-      ),
-      MathUtils.damp(0, 1, 1, deltaTime * LERP_SCALE),
-    ),
-    'ZYX',
-  );
-  euler.x = clampByRadian(euler.x, -HEAD_CLAMP_ANGLE, HEAD_CLAMP_ANGLE);
-  euler.y = clampByRadian(euler.y, -HEAD_CLAMP_ANGLE, HEAD_CLAMP_ANGLE);
-  euler.z = clampByRadian(euler.z, -HEAD_CLAMP_ANGLE, HEAD_CLAMP_ANGLE);
+  matrix.lookAt(position, position2, vector3.set(0, 1, 0));
+  vector3.set(0, 0, 1).applyMatrix4(matrix);
   bone.applyQuaternion(
-    rotation2.setFromEuler(euler).multiply(
-      rotation3
-      .setFromRotationMatrix(bone.matrix)
-      .invert()
-      .premultiply(rotation)
-      .invert(),
+    rotation2.copy(rotation).slerp(
+      vector3.z < 0 ? rotation3.identity() : rotation3.setFromEuler(
+        euler.setFromRotationMatrix(matrix).set(
+          clampByRadian(euler.x, -HEAD_CLAMP_ANGLE, HEAD_CLAMP_ANGLE),
+          clampByRadian(euler.y, -HEAD_CLAMP_ANGLE, HEAD_CLAMP_ANGLE),
+          clampByRadian(euler.z, -HEAD_CLAMP_ANGLE, HEAD_CLAMP_ANGLE),
+        ),
+      ),
+      MathUtils.damp(0, 1, HEAD_ROTATE_DAMP, deltaTime * LERP_SCALE),
+    ).multiply(
+      rotation3.setFromRotationMatrix(bone.matrix),
+    ).multiply(
+      rotation3.copy(rotation).invert(),
     ),
   );
 }
@@ -125,30 +139,32 @@ function updateEyeBlink(model: VRM, deltaTime: number) {
   if (!model.blendShapeProxy) return;
   let v = blinkDelays.get(model);
   if (v == null || v < -BLINK_DURATION)
-    v = MathUtils.lerp(MIN_BLINK_DELAY, MAX_BLINK_DEALY, Math.random());
+    v = MathUtils.randFloat(MIN_BLINK_DELAY, MAX_BLINK_DEALY);
   else
     v -= deltaTime;
   blinkDelays.set(model, v);
-  if (v <= 0) {
-    v = -v;
-    if (v < BLINK_DURATION / 2)
-      v = MathUtils.lerp(0, 1, v / BLINK_DURATION * 2);
-    else
-      v = MathUtils.lerp(0, 1, 2 - v / BLINK_DURATION * 2);
-    model.blendShapeProxy.setValue(VRMSchema.BlendShapePresetName.Blink, v);
-  } else {
-    model.blendShapeProxy.setValue(VRMSchema.BlendShapePresetName.Blink, 0);
-  }
+  model.blendShapeProxy.setValue(
+    VRMSchema.BlendShapePresetName.Blink,
+    v > 0 ? 0 : MathUtils.pingpong(-v, BLINK_DURATION / 2) * 2 / BLINK_DURATION,
+  );
 }
 
 function updateIdlePose(model: VRM, deltaTime: number) {
   if (!model.humanoid) return;
+  const totalTime = (totalTimes.get(model) || 0) + deltaTime;
+  totalTimes.set(model, totalTime);
   for (const [bone, rotation] of idlePose) {
     const node = model.humanoid.getBoneNode(bone);
+    let finalRotation = rotation;
+    const breathRotation = breathPose.get(bone);
+    if (breathRotation)
+      finalRotation = rotation2
+      .copy(rotation)
+      .slerp(breathRotation, MathUtils.pingpong(totalTime, BREATH_CYCLE));
     if (node) node.setRotationFromQuaternion(
-      rotation2
+      rotation3
       .setFromRotationMatrix(node.matrix)
-      .slerp(rotation, MathUtils.lerp(0, 1, deltaTime * LERP_SCALE)),
+      .slerp(finalRotation, MathUtils.lerp(0, 1, deltaTime * LERP_SCALE)),
     );
   }
 }
