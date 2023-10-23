0d98c6cb14479db063b2243d936c99484809899d
syui <syui@syui.ai>
2023-10-23 15:28:41 +0900

    update renderer sRGBEncoding

--- src/worker/scene/renderer.ts
+++ src/worker/scene/renderer.ts
@@ -1,11 +1,13 @@
 import { WebGLRenderer } from 'three';
+import * as THREE from 'three';
 
 export let renderer: WebGLRenderer | undefined;
 
 export function init(canvas: HTMLCanvasElement | OffscreenCanvas) {
-  if (!renderer) renderer = new WebGLRenderer({
-    antialias: true,
-    canvas,
-  });
-  return renderer;
+	if (!renderer) renderer = new WebGLRenderer({
+		antialias: true,
+		canvas,
+	});
+	renderer.outputEncoding = THREE.sRGBEncoding;
+	return renderer;
 }
